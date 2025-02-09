c66408dd67e9574ddd657ebbae4d7e21f30830a4
stevenbenalcazar <sabenalcazarm@uce.edu.ec>
2025-02-08 20:20:36 -0500

    .........

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('../config/database'); // Asegúrate de que esto apunta a tu configuración
 
 const User = sequelize.define('User', {
   id: {
